df8c64c705cf72195a4932d8d895d1e44a7c1808
Ram <ratram2010@gmail.com>
2023-07-24 22:44:10 +0100

    Roboshop First Commit

---
+++ mongodb.sh
@@ -0,0 +1,5 @@
+cp mongodb.repo  /etc/yum.repos.d/mongo.repo
+yum install mongodb-org -y
+#Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+systemctl enable mongod
+systemctl restart mongod
